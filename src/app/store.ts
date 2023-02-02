5c692ad249242957c4980da97993da2600600646
kgtulin <kgtulin@yandex.ru>
2023-02-02 17:39:06 +0300

    Initial commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import {todosReducer} from "../features/todos/todosSlice";
 import {filterReducer} from "../features/filter/filterSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     todos: todosReducer,
     filter: filterReducer
   },
